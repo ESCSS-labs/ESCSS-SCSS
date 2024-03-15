4cad78e875b76e7625c22a10670422caf27e84f5
mike <cspkno005@gmail.com>
2024-03-15 08:04:49 +0800

    done part-layout

--- test/sass.test.js
+++ test/sass.test.js
@@ -1,7 +1,7 @@
 const path = require('node:path');
 const sassTrue = require('sass-true');
 
-const sassFile = path.join(__dirname, 'test.scss');
+const sassFile = path.join(__dirname, '_test.scss');
 
 sassTrue.runSass({ describe, it }, sassFile);
 
