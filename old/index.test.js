8a73149af2668a3ee48930ee01c7685bb4f59c05
mike <cspkno005@gmail.com>
2024-03-22 10:21:32 +0800

    add prettier lib

--- old/index.test.js
+++ old/index.test.js
@@ -1,7 +1,6 @@
-const path = require('node:path');
-const sassTrue = require('sass-true');
+const path = require("node:path");
+const sassTrue = require("sass-true");
 
-const sassFile = path.join(__dirname, '_test.scss');
+const sassFile = path.join(__dirname, "_test.scss");
 
 sassTrue.runSass({ describe, it }, sassFile);
-
