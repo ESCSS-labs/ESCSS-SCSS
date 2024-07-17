41d26a1538b4bda4bcf8f1dce4679474b5dce084
mike <cspkno005@gmail.com>
2024-07-17 21:43:05 +0800

    refac all

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,6 +1,6 @@
-const path = require("node:path");
-const sassTrue = require("sass-true");
+const path = require('node:path');
+const sassTrue = require('sass-true');
 
-const testStyle = path.join(__dirname, "_index.scss");
+const testStyle = path.join(__dirname, '_index.scss');
 
 sassTrue.runSass({ describe, it }, testStyle);
